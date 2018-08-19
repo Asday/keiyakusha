ad06b6eb5e9d423fa3494189d2597ea31a8e764c
asday <sara.and.zuka@gmail.com>
2018-08-19 18:20:30 +0100

    Shifted table stuff into a table module.
    Where it belongs.

---
+++ src/keiyakusha/timing_website/tables.py
@@ -0,0 +1,35 @@
+from django_tables2 import Column, Table
+
+from timing.models import TimeEntry
+
+
+class TimeEntryTable(Table):
+    task = Column(order_by=(
+        'task__project__client__name',
+        'task__project__name',
+        'task__external_reference',
+    ))
+
+    class Meta:
+        # TODO: May be better to have this in settings?
+        template_name = 'django_tables2/bootstrap4.html'
+        model = TimeEntry
+        fields = (
+            'start_date',
+            'start_time',
+            'end_time',
+            'duration',
+            'task',
+        )
+
+    def _order_by_date(self, qs, is_descending):
+        return (qs.order_by(f'{"-" if is_descending else ""}start'), True)
+
+    def order_start_date(self, qs, is_descending):
+        return self._order_by_date(qs, is_descending)
+
+    def order_start_time(self, qs, is_descending):
+        return self._order_by_date(qs, is_descending)
+
+    def order_end_time(self, qs, is_descending):
+        return self._order_by_date(qs, is_descending)
--- src/keiyakusha/timing_website/views.py
+++ src/keiyakusha/timing_website/views.py
@@ -1,41 +1,11 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView
 
-from django_tables2 import Column, RequestConfig, Table
+from django_tables2 import RequestConfig
 
 from timing.models import TimeEntry
 
-
-class TimeEntryTable(Table):
-    task = Column(order_by=(
-        'task__project__client__name',
-        'task__project__name',
-        'task__external_reference',
-    ))
-
-    class Meta:
-        # TODO: May be better to have this in settings?
-        template_name = 'django_tables2/bootstrap4.html'
-        model = TimeEntry
-        fields = (
-            'start_date',
-            'start_time',
-            'end_time',
-            'duration',
-            'task',
-        )
-
-    def _order_by_date(self, qs, is_descending):
-        return (qs.order_by(f'{"-" if is_descending else ""}start'), True)
-
-    def order_start_date(self, qs, is_descending):
-        return self._order_by_date(qs, is_descending)
-
-    def order_start_time(self, qs, is_descending):
-        return self._order_by_date(qs, is_descending)
-
-    def order_end_time(self, qs, is_descending):
-        return self._order_by_date(qs, is_descending)
+from .tables import TimeEntryTable
 
 
 class TimingView(LoginRequiredMixin, ListView):
