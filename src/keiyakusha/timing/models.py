ecc9923d2863232e014a55aee8f857bfc880e18b
asday <sara.and.zuka@gmail.com>
2018-08-19 01:29:12 +0100

    Got real data for current timers.

--- src/keiyakusha/timing/models.py
+++ src/keiyakusha/timing/models.py
@@ -1,4 +1,134 @@
+from calendar import weekday
+from collections import defaultdict
+from datetime import timedelta
+from decimal import Decimal
+
 from django.db import models
+from django.utils import timezone
+
+import attr
+
+
+@attr.s
+class TimerData(object):
+    duration = attr.ib()
+    totals = attr.ib()
+
+    @classmethod
+    def from_queryset(cls, qs):
+        # TODO: Test.
+        return cls(
+            duration=qs.total_duration(),
+            totals=[
+                Total(amount=amount, currency=currency)
+                for currency, amount in qs.total_amounts().items()
+            ],
+        )
+
+    @classmethod
+    def from_instance(cls, time_entry):
+        # TODO: Test.
+        return cls(
+            duration=time_entry.duration_so_far,
+            totals=[
+                Total(amount=time_entry.amount, currency=time_entry.currency)
+            ],
+        )
+
+    def render_totals(self):
+        # TODO: Test.
+        totals = defaultdict(Decimal)
+        for total in self.totals:
+            totals[total.currency] += total.amount
+
+        ordered_totals = sorted(
+            totals.items(),
+            key=lambda item: item[1],
+            reverse=True,
+        )
+
+        rendered_totals = (
+            ''.join((currency, str(amount.quantize(Decimal('0.01')))))
+            for currency, amount in ordered_totals
+        )
+
+        return ' '.join(rendered_totals)
+
+
+@attr.s
+class Total(object):
+    amount = attr.ib()
+    currency = attr.ib()
+
+
+class TimeEntryQuerySet(models.QuerySet):
+
+    def uninvoiced(self):
+        return self.filter(invoice=None)
+
+    def running_tasks(self):
+        return self.filter(duration=None)
+
+    def from_this_week(self, user=None):
+        # TODO: Test.
+        # TODO: Might not be the best place for getting week start.
+        # TODO: Might not be the best place for getting last Monday.
+        week_start_on_monday = True
+        if user is not None and user.profile is not None:
+            week_start_on_monday = user.profile.week_start_on_monday
+
+        today = timezone.now().date()
+        current_weekday = weekday(today.year, today.month, today.day)
+
+        if not week_start_on_monday:
+            # We're going to subtract the weekday from the current date
+            # to get the start of the week's date.  If the user has for
+            # some bizarre reason decided that Sunday is the start of
+            # the week, shift `current_weekday` such that 0 == Sunday
+            # instead of Monday.
+            current_weekday += 1
+            current_weekday %= 7
+
+        last_monday = today - timedelta(days=current_weekday)
+
+        return self.filter(
+            start__day__gte=last_monday.day,
+            start__month__gte=last_monday.month,
+            start__year__gte=last_monday.year,
+        )
+
+    def from_today(self):
+        today = timezone.now().date()
+
+        return self.filter(
+            start__day__gte=today.day,
+            start__month__gte=today.month,
+            start__year__gte=today.year,
+        )
+
+    def total_duration(self):
+        # TODO: Test.
+        return self.aggregate(models.Sum('duration'))['duration__sum']
+
+    def total_amounts(self):
+        # TODO: Test.
+        totals = defaultdict(Decimal)
+        for time_entry in self:
+            totals[time_entry.engagement.currency] += time_entry.amount
+
+        return totals
+
+    def data(self):
+        return TimerData.from_queryset(self)
+
+
+class TimeEntryManager(models.Manager.from_queryset(TimeEntryQuerySet)):
+
+    def current_task(self, user):
+        try:
+            return self.running_tasks().filter(engagement__user=user).get()
+        except self.model.DoesNotExist:
+            return None
 
 
 class TimeEntry(models.Model):
@@ -24,6 +154,8 @@ class TimeEntry(models.Model):
         related_name='time_entries',
     )
 
+    objects = TimeEntryManager()
+
     class Meta:
         verbose_name_plural = 'time entries'
 
@@ -33,3 +165,22 @@ class TimeEntry(models.Model):
     @property
     def rendered_note(self):
         return f' {self.note}' if self.note else ''
+
+    @property
+    def duration_so_far(self):
+        return timezone.now() - self.start
+
+    @property
+    def amount(self):
+        # TODO: Test.
+        duration = self.duration or self.duration_so_far
+        secondly_rate = self.engagement.rate / 60 / 60
+
+        return duration.seconds * secondly_rate
+
+    @property
+    def currency(self):
+        return self.engagement.currency
+
+    def as_data(self):
+        return TimerData.from_instance(self)
--- src/keiyakusha/website/views/timing.py
+++ src/keiyakusha/website/views/timing.py
@@ -1,44 +1,7 @@
-from collections import defaultdict
-from datetime import timedelta
-from decimal import Decimal
-
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
 
-import attr
-
-
-# Will likely move closer to where the data comes from when the
-# time comes.  For now it can be here to fill with test data.
-@attr.s
-class TimerData(object):
-    duration = attr.ib()
-    totals = attr.ib()
-
-    def render_totals(self):
-        # TODO: Test.
-        totals = defaultdict(Decimal)
-        for total in self.totals:
-            totals[total.currency] += total.amount
-
-        ordered_totals = sorted(
-            totals.items(),
-            key=lambda item: item[1],
-            reverse=True,
-        )
-
-        rendered_totals = (
-            ''.join((str(total_item) for total_item in total))
-            for total in ordered_totals
-        )
-
-        return ' '.join(rendered_totals)
-
-
-@attr.s
-class Total(object):
-    amount = attr.ib()
-    currency = attr.ib()
+from timing.models import TimeEntry
 
 
 # TODO: Should this subclass `ListView` instead?
@@ -46,42 +9,24 @@ class TimingView(LoginRequiredMixin, TemplateView):
     template_name = 'timing/index.html'
 
     def get_uninvoiced_data(self):
-        # TODO: Get real data.
-        return TimerData(
-            duration=timedelta(hours=73, minutes=20, seconds=3),
-            totals=[
-                Total(amount=Decimal('10.00'), currency='AUD'),
-                Total(amount=Decimal('15.00'), currency='AUD'),
-                Total(amount=Decimal('20.00'), currency='GBP'),
-            ]
-        )
+        return TimeEntry.objects \
+            .filter(engagement__user=self.request.user) \
+            .uninvoiced() \
+            .data()
 
     def get_this_week_data(self):
-        # TODO: Get real data.
-        return TimerData(
-            duration=timedelta(hours=12, minutes=10),
-            totals=[
-                Total(amount=Decimal('15.00'), currency='AUD'),
-                Total(amount=Decimal('20.00'), currency='GBP'),
-            ]
-        )
+        return TimeEntry.objects \
+            .filter(engagement__user=self.request.user) \
+            .from_this_week() \
+            .data()
 
     def get_today_data(self):
-        # TODO: Get real data.
-        return TimerData(
-            duration=timedelta(hours=3, minutes=40, seconds=35),
-            totals=[
-                Total(amount=Decimal('15.00'), currency='AUD'),
-                Total(amount=Decimal('20.00'), currency='GBP'),
-            ]
-        )
+        return TimeEntry.objects \
+            .filter(engagement__user=self.request.user) \
+            .from_today() \
+            .data()
 
     def get_current_task_data(self):
-        # TODO: Get real data.
-        # return None
-        return TimerData(
-            duration=timedelta(hours=1),
-            totals=[
-                Total(amount=Decimal('5.00'), currency='GBP'),
-            ]
-        )
+        current_task = TimeEntry.objects.current_task(self.request.user)
+
+        return current_task and current_task.as_data()
