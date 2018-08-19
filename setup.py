1b3e0edbdc3203f8504bd4fe6b4e1f03a5a2e22b
asday <sara.and.zuka@gmail.com>
2018-08-19 16:57:28 +0100

    Installed `django_tables2`.

--- setup.py
+++ setup.py
@@ -35,6 +35,7 @@ setup(
     ],
     install_requires=[
         'django==2.1',
+        'django_tables2==2.0.0a5',
         'django-widget-tweaks==1.4.2',
         'unicode-slugify==0.1.3',
     ],
