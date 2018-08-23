4498d6f6a643c5b09ab193365611de17beb0444a
asday <sara.and.zuka@gmail.com>
2018-08-23 04:52:41 +0100

    Added timezones to profiles.

--- src/keiyakusha/users/models.py
+++ src/keiyakusha/users/models.py
@@ -2,10 +2,16 @@ from django.conf import settings
 from django.db import models
 
 from annoying.fields import AutoOneToOneField
+from pytz import all_timezones
 
 
 class UserProfile(models.Model):
     week_start_on_monday = models.BooleanField(default=True)
+    timezone = models.CharField(
+        max_length=max((len(timezone) for timezone in all_timezones)),
+        choices=((timezone, timezone) for timezone in all_timezones),
+        default='UTC',
+    )
 
     user = AutoOneToOneField(
         settings.AUTH_USER_MODEL,
