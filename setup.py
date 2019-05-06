297fe1cb5bc270e6c2f1c71e0a4d979288a1b5cd
asday <sara.and.zuka@gmail.com>
2019-05-06 21:28:25 +0100

    flake8.

--- setup.py
+++ setup.py
@@ -23,7 +23,8 @@ setup(
     include_package_data=True,
     zip_safe=False,
     classifiers=[
-        # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers  # noqa
+        # Complete classifier list:
+        # http://pypi.python.org/pypi?%3Aaction=list_classifiers
         'Development Status :: 1 - Planning',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
